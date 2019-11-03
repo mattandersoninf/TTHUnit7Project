1336b22c6095968481eb281904b514e9c1db7616
Matt Anderson <matt.anderson.inf@gmail.com>
2019-11-03 14:42:09 -0500

    Search input progress, chart progress

--- js/app.js
+++ js/app.js
@@ -18,6 +18,8 @@ let lineChart = new Chart(trafficChart, {
         labels:['16-22','23-29','30-5','6-12','13-19','20-26','27-3','4-10','11-17','18-24','25-31'],
         datasets:[{
             label: 'Traffic',
+            // straigntens out the line
+            lineTension: 0,
             data:['500',
                   '1000',
                   '750',
